5adb6fd623b6ce755867b1a369f3ac4d8dd98139
Giacomo Nanni <gn.nanni@gmail.com>
2025-09-23 16:31:14 +0200

    chore localized archive

--- src/lib/components/Featured.svelte
+++ src/lib/components/Featured.svelte
@@ -3,6 +3,7 @@
 	import { m } from '$lib/paraglide/messages';
 	import Card from '$lib/components/Card.svelte';
 	import NewsletterPanel from '$lib/components/NewsletterPanel.svelte';
+	import { localizeHref } from '$lib/paraglide/runtime';
 
 	const featured = await query('featured');
 	const newsletters = await query('newsletters');
@@ -10,9 +11,12 @@
 
 <section class="grid w-svw gap-2 lg:grid-cols-[1fr_320px]">
 	<div class="space-y-4 p-2">
-		<div class="columns-1 gap-2 [column-fill:balance] sm:columns-2 lg:columns-4">
+		<div class="coluhens-1 gap-2 [column-fill:balance] sm:columns-2 lg:columns-4">
 			<div class="mb-4 break-inside-avoid">
-				<a href="/archive" class="block h-full bg-brand-green-200 p-4 hover:bg-brand-black">
+				<a
+					href={localizeHref('/archive/')}
+					class="block h-full bg-brand-green-200 p-4 hover:bg-brand-black"
+				>
 					<h2 class="text-4xl leading-none font-medium">
 						<span class="inline bg-white px-1">{m.explore_archive()}</span>
 					</h2>

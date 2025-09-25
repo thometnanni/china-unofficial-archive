07803f8362220a8fff0234db8e00e070358b6625
Giacomo Nanni <gn.nanni@gmail.com>
2025-09-25 11:49:25 +0200

    chore: search padding

--- src/lib/components/Search.svelte
+++ src/lib/components/Search.svelte
@@ -1,11 +1,17 @@
 <script>
-	export let value = '';
+	let { value } = $props();
+
 	import { m } from '$lib/paraglide/messages.js';
 </script>
 
-<section id="search">
+<section id="search" class="mt-10 border-b">
 	<div class="visible">
-		<input type="text" class="w-full border-transparent" bind:value placeholder={m.search_placeholder()} />
+		<input
+			type="text"
+			class="w-full border-transparent"
+			bind:value
+			placeholder={m.search_placeholder()}
+		/>
 	</div>
 	<div class="hidden">
 		<select>

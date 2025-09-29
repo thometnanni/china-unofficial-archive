3be78bbb6aac1b714d89b7c3a5c10c4fd601bef0
Giacomo Nanni <gn.nanni@gmail.com>
2025-09-29 11:50:48 +0200

    fix margin

--- src/lib/components/NewsletterPanel.svelte
+++ src/lib/components/NewsletterPanel.svelte
@@ -2,10 +2,11 @@
 	let { items = [] } = $props();
 </script>
 
-<div class="relative inline-block pt-8 text-black">
-	<h3 class="absolute top-0 right-0 z-10 bg-brand-yellow px-2 py-1 text-xl">Newsletter</h3>
-
-	<section class="w-fit bg-brand-yellow">
+<div class="inline-block text-black">
+	<div class="flex flex-row-reverse items-end">
+		<div class="z-10 -mb-2 bg-brand-yellow px-4 py-1 text-xl">Newsletter</div>
+	</div>
+	<section class=" bg-brand-yellow pt-4 pb-2">
 		<ul class="space-y-1">
 			{#each items.slice(0, 10) as n}
 				<li>
@@ -13,7 +14,7 @@
 						href={n.url}
 						class="flex w-full items-start gap-2 px-2 py-1 hover:bg-black hover:text-white"
 					>
-						<div class="text-sm">
+						<div class="text-base">
 							<div class="opacity-70">{new Date(n.date).toLocaleDateString()}</div>
 							<div class="leading-snug">{n.title}</div>
 						</div>
@@ -24,7 +25,7 @@
 
 		<a
 			href="/newsletter"
-			class="mt-3 block w-full pb-2 text-center text-sm hover:bg-black hover:text-white"
+			class="mt-3 block w-full rounded pb-2 text-center text-sm hover:bg-black hover:text-white"
 		>
 			See all
 		</a>

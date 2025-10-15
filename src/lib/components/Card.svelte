9cb4c07c95cfcc28c79faeaa7f615b8fc671115b
Fidel Thomet <git@ft0.ch>
2025-10-15 12:27:07 +0200

    remove unused basecard component

--- src/lib/components/Card.svelte
+++ src/lib/components/Card.svelte
@@ -2,17 +2,14 @@
 	import CardCreator from '$lib/components/cards/CardCreator.svelte';
 	import CardObject from '$lib/components/cards/CardObject.svelte';
 	import CardFilter from '$lib/components/cards/CardFilter.svelte';
-	import BaseCard from '$lib/components/cards/BaseCard.svelte';
 	let { item, i = 0 } = $props();
 	// console.log(item);
 </script>
 
 {#if item.type === 'creator'}
 	<CardCreator {item} {i} />
-{:else if item.type === 'theme' || item.type === 'era' || item.type === 'objectType'}
-	<CardFilter {item} {i} />
 {:else if item.type === 'object'}
 	<CardObject {item} {i} />
 {:else}
-	<BaseCard {item} {i} />
+	<CardFilter {item} {i} />
 {/if}

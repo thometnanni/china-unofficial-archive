faae2a0d1142f0ac61022e98382fd58bf8931efc
Giacomo Nanni <gn.nanni@gmail.com>
2025-10-21 17:58:38 +0200

    feat favicon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,12 @@
 <script>
 	import '../app.css';
-	import favicon from '$lib/assets/favicon.svg';
+	// import favicon from '$lib/assets/favicon.svg';
 
 	let { children } = $props();
 </script>
 
 <svelte:head>
-	<link rel="icon" href={favicon} />
+	<link rel="icon" href="/favicon.png" />
 </svelte:head>
 
 {@render children?.()}

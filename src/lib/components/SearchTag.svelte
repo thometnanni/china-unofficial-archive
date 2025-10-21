c740e4769b3fca05306c7f48378b98a6ca151ea4
Fidel Thomet <git@ft0.ch>
2025-10-21 14:57:47 +0200

    Feat: SearchTag component; customise TextOutlined

---
+++ src/lib/components/SearchTag.svelte
@@ -0,0 +1,35 @@
+<script>
+	import TextOutlined from './TextOutlined.svelte';
+
+	let { item } = $props();
+</script>
+
+<span class={['search-tag', item.type]} role="button">
+	<TextOutlined>{item.title ?? item.value}</TextOutlined>
+</span>
+
+<style>
+	.search-tag {
+		cursor: default;
+		--color-outlined-border: var(--color-type-default);
+		--color-outlined-bg: var(--color-type-default);
+
+		&:hover {
+			--color-outlined-bg: var(--color-white);
+		}
+	}
+	.object {
+		--color-outlined-border: var(--color-type-object);
+		--color-outlined-bg: var(--color-type-object);
+	}
+
+	.creator {
+		--color-outlined-border: var(--color-type-creator);
+		--color-outlined-bg: var(--color-type-creator);
+	}
+
+	.era {
+		--color-outlined-border: var(--color-type-era);
+		--color-outlined-bg: var(--color-type-era);
+	}
+</style>
--- src/lib/components/TextOutlined.svelte
+++ src/lib/components/TextOutlined.svelte
@@ -2,18 +2,23 @@
 	let { children, class: className = '', as = 'div' } = $props();
 </script>
 
-<svelte:element this={as} class={`grid ${className}`}>
-	<div
-		class="col-[1/2] row-[1/2] leading-[var(--lh-tight)] text-balance text-black"
-		aria-hidden="true"
-	>
-		<span class="border border-card-primary bg-white box-decoration-clone px-1">
+<svelte:element this={as} class={`text-outlined grid ${className}`}>
+	<div class="col-[1/2] row-[1/2] leading-[var(--lh-tight)] text-balance" aria-hidden="true">
+		<span class="border box-decoration-clone px-1">
 			{@render children?.()}
 		</span>
 	</div>
-	<div class="col-[1/2] row-[1/2] leading-[var(--lh-tight)] text-balance text-black">
-		<span class="border-r border-l border-card-primary bg-white box-decoration-clone px-1">
+	<div class="col-[1/2] row-[1/2] leading-[var(--lh-tight)] text-balance">
+		<span class="border-r border-l box-decoration-clone px-1">
 			{@render children?.()}
 		</span>
 	</div>
 </svelte:element>
+
+<style>
+	.text-outlined > div > span {
+		border-color: var(--color-outlined-border, var(--color-card-primary));
+		color: var(--color-outlined-text, black);
+		background-color: var(--color-outlined-bg, white);
+	}
+</style>

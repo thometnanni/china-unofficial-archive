29e4a0c5f64e4bdb44a161ff482f59659be1615b
Giacomo Nanni <gn.nanni@gmail.com>
2025-10-21 16:45:56 +0200

    chore splash text

--- src/lib/components/Splash.svelte
+++ src/lib/components/Splash.svelte
@@ -70,37 +70,25 @@
 		{/if}
 	</div>
 
-	{#if lang === 'zh'}
-		<h2
-			class="en z-10 mx-auto mt-12 max-w-[640px] px-2 text-left indent-10 text-3xl text-black sm:absolute sm:right-4 sm:bottom-62 sm:mt-0 sm:text-left sm:text-4xl"
-		>
-			<span class="bg-gray-100">
-				<TextOutlined>{m.slogan(null, { locale: 'en' })}</TextOutlined>
-			</span>
-		</h2>
-		<h2
-			class="zh z-10 mx-auto mt-6 mb-28 max-w-[640px] px-2 text-left indent-10 text-3xl text-black sm:absolute sm:top-60 sm:left-4 sm:mt-0 sm:mb-0 sm:text-4xl"
-		>
-			<TextOutlined>
-				唯一非营利性的 <span class="text-nowrap">中国独立思想档案库</span>
-			</TextOutlined>
-		</h2>
-	{:else}
-		<h2
-			class="en z-10 mx-auto mt-12 max-w-[640px] px-2 text-left indent-10 text-3xl text-black sm:absolute sm:right-4 sm:bottom-62 sm:mt-0 sm:text-left sm:text-4xl"
-		>
-			<span class="bg-gray-100">
+	{#each lang === 'zh' ? ['zh', 'en'] : ['en', 'zh'] as l}
+		{#if l === 'zh'}
+			<h2
+				class="zh z-10 mx-auto mt-6 mb-28 max-w-[640px] px-2 text-left indent-10 text-4xl text-black sm:absolute sm:top-60 sm:left-4 sm:mt-0 sm:mb-0 sm:text-4xl"
+			>
 				<TextOutlined>
 					唯一非营利性的 <span class="text-nowrap">中国独立思想档案库</span>
 				</TextOutlined>
-			</span>
-		</h2>
-		<h2
-			class="zh z-10 mx-auto mt-6 mb-28 max-w-[640px] px-2 text-left indent-10 text-3xl text-black sm:absolute sm:top-60 sm:left-4 sm:mt-0 sm:mb-0 sm:text-4xl"
-		>
-			<TextOutlined>{m.slogan(null, { locale: 'en' })}</TextOutlined>
-		</h2>
-	{/if}
+			</h2>
+		{:else}
+			<h2
+				class="en z-10 mx-auto mt-12 max-w-[640px] px-2 text-left indent-10 text-4xl text-black sm:absolute sm:right-4 sm:bottom-62 sm:mt-0 sm:text-left sm:text-4xl"
+			>
+				<span class="bg-gray-100">
+					<TextOutlined>{m.slogan(null, { locale: 'en' })}</TextOutlined>
+				</span>
+			</h2>
+		{/if}
+	{/each}
 
 	<div class="z-20 mb-4 flex gap-4">
 		<!-- <button

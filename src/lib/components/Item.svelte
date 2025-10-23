b59b85c240cc3463d1ac4d3b9fec7b5fb149808a
Giacomo Nanni <gn.nanni@gmail.com>
2025-10-23 11:23:41 +0200

    chore basic item setup

--- src/lib/components/Item.svelte
+++ src/lib/components/Item.svelte
@@ -4,6 +4,8 @@
 	import { m } from '$lib/paraglide/messages.js';
 	import { localizeHref } from '$lib/paraglide/runtime';
 	import { creator } from 'd3-selection';
+	import TextOutlined from '$lib/components/TextOutlined.svelte';
+	import ImageFilter from '$lib/components/ImageFilter.svelte';
 
 	const { item } = $props();
 	console.log(item.description);
@@ -11,21 +13,34 @@
 
 <section class="flex flex-col gap-4 p-2">
 	{#if item.thumbnail && item.type == 'creator'}
-		<div class="{item.type} card">
-			<img
+		<div class="{item.type} card max-h-[200px] w-fit border">
+			<!-- <img
 				src={item.thumbnail}
 				alt=""
 				crossorigin="anonymous"
-				class="block max-h-80 w-full object-cover"
+				class="object-fit block max-h-[200px] border"
+			/> -->
+
+			<ImageFilter
+				src={item.thumbnail}
+				alt={item.title}
+				color="var(--color-brand-primary)"
+				fit="contain"
 			/>
 		</div>
 	{/if}
 
-	<h2 class="text-2xl">{item.title}</h2>
-	<h3 class="text-xl">{item.titleAlt}</h3>
-	<p>
+	<h2 class="text-2xl">
+		<TextOutlined>{item.title}</TextOutlined>
+	</h2>
+	{#if item.titleAlt?.length > 0}
+		<h3 class="text-xl">
+			<TextOutlined>{item.titleAlt}</TextOutlined>
+		</h3>
+	{/if}
+	<!-- <p>
 		{item.published}
-	</p>
+	</p> -->
 	<p>
 		{#each item.creator as creator}
 			<a class="person bg-gray-200 px-2 py-1" href={localizeHref(`/archive/${creator.id}`)}
@@ -41,20 +56,23 @@
 			>
 		{/each}
 	</p>
-	<p class="text-xs">
+	<div>
 		{#each [...(item.theme ?? []), ...(item.era ?? [])] as theme}
 			<a
-				class="mr-1 filter {item.type} box-decoration-clone px-1"
+				class="filter {item.type} mr-1 bg-gray-200 p-1 text-xs leading-loose"
 				href={localizeHref(`/archive/${theme.id}`)}
 			>
 				{theme.title}
 			</a>
 		{/each}
-	</p>
-	<div class="whitespace-pre-wrap">
+	</div>
+	<div class="text-xl whitespace-pre-wrap">
 		{@html item.description}
 	</div>
 </section>
 
 <style>
+	.card {
+		--color-card-primary: var(--color-type-creator);
+	}
 </style>

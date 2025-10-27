1d80794a241400ead327b9650f966ed817519b4b
Giacomo Nanni <gn.nanni@gmail.com>
2025-10-27 16:48:55 +0100

    feat datatype card colour

--- src/lib/components/cards/CardObject.svelte
+++ src/lib/components/cards/CardObject.svelte
@@ -1,6 +1,6 @@
 <script>
 	import ImageFilter from '$lib/components/ImageFilter.svelte';
-	import { derived } from 'svelte/store';
+	// import { derived } from 'svelte/store';
 	import TextOutlined from '../TextOutlined.svelte';
 	let { item, href } = $props();
 	let isPortrait = $state(true);
@@ -10,13 +10,22 @@
 	function onRatio(e) {
 		isPortrait = e.detail.isPortrait;
 	}
+	const dataType = $derived(
+		String(item?.objectType?.[0]?.title || '')
+			.toLowerCase()
+			.trim()
+	);
+
+	// console.log(dataType);
 </script>
 
-<a {href} class="card col-span-3 row-span-3 grid grid-cols-3 grid-rows-3 gap-8">
+<a
+	{href}
+	class="card col-span-3 row-span-3 grid grid-cols-3 grid-rows-3 gap-8"
+	data-type={dataType}
+>
 	<div
-		class={`${
-			isPortrait ? 'col-[1/3] row-[1/4]' : 'col-[1/4] row-[1/3]'
-		} border border-card-primary`}
+		class={`${isPortrait ? 'col-[1/3] row-[1/4]' : 'col-[1/4] row-[1/3]'} border border-card-primary`}
 	>
 		<div class="relative h-full p-1">
 			<ImageFilter
@@ -26,11 +35,10 @@
 				fit="cover"
 				objectPosition="center center"
 			/>
-
 			{#if item.objectType}
 				<div class="absolute top-1 left-[calc(var(--spacing)_*_-4)] text-balance">
 					{#each item.objectType as objectType}
-						<TextOutlined class="mb-1">{objectType.title}</TextOutlined>
+						<TextOutlined class="mb-1 text-card-primary">{objectType.title}</TextOutlined>
 					{/each}
 				</div>
 			{/if}
@@ -52,5 +60,26 @@
 <style>
 	.card {
 		--color-card-primary: var(--color-type-object);
+	}
+	.card[data-type='book'] {
+		--color-card-primary: var(--color-type-object-book);
+	}
+	.card[data-type='article'] {
+		--color-card-primary: var(--color-type-object-article);
+	}
+	.card[data-type='periodical'] {
+		--color-card-primary: var(--color-type-object-periodical);
+	}
+	.card[data-type='newsletter'] {
+		--color-card-primary: var(--color-type-object-newsletter);
+	}
+	.card[data-type='video'] {
+		--color-card-primary: var(--color-type-object-video);
+	}
+	.card[data-type='film'] {
+		--color-card-primary: var(--color-type-object-video);
+	}
+	.card[data-type='film and video'] {
+		--color-card-primary: var(--color-type-object-video);
 	}
 </style>

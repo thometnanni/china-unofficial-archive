bd42585b053e9fb55f94856ebf0538d981a8b94a
Giacomo Nanni <gn.nanni@gmail.com>
2025-11-12 10:17:54 +0100

    fix: grid elements not spanning the entire width

--- src/lib/components/Items.svelte
+++ src/lib/components/Items.svelte
@@ -8,7 +8,7 @@
 
 <div class="items space-y-4 p-8">
 	<div
-		class="grid grid-flow-dense auto-rows-[var(--grid-cell-size)] grid-cols-[repeat(auto-fit,minmax(var(--grid-cell-size),1fr))] gap-8"
+		class="grid grid-flow-dense auto-rows-[var(--grid-cell-size)] grid-cols-[repeat(auto-fit,minmax(var(--grid-cell-size),max-content))] justify-center gap-8"
 	>
 		{#each items as item, i}
 			<Card {item} {i} />
